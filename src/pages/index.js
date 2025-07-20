e50d16c8ebd66a18d96f1e7b0ce5d8f5e9997567
Aradhya <aradhyaverma954@gmail.com>
2025-07-20 11:11:42 +0530

    Added ServicePackages, SmartSolutions, and Navigation pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import dynamic from "next/dynamic";
+
 import OurAdvantages from "./OurAdvantages";
 import AboutSection from "./AboutSection";
 
@@ -11,6 +12,15 @@ const HeroBanner = dynamic(() => import("./HeroBannerSection"), {
   loading: () => <div className="h-screen bg-black" />,
 });
 
+const ServicePackages = dynamic(() => import("./ServicePackages"),
+ { ssr: false });
+
+const SmartSolutions = dynamic(() => import("./SmartSolutions"),
+ { ssr: false });
+
+const Navigation = dynamic(() => import("./Navigation"),
+ { ssr: false });
+
 export default function Home() {
   return (
     <div className="min-h-screen bg-black">
@@ -18,6 +28,13 @@ export default function Home() {
       <HeroBanner />
       <OurAdvantages/>
       <AboutSection/>
+      <Navigation />
+      <div id="service-packages">
+        <ServicePackages />
+      </div>
+      <div id="smart-solutions">
+        <SmartSolutions />
+      </div>
     </div>
   );
 }
