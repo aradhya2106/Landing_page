0bf07aa2cd5864e28c7f4bd7a536bfb821f20be7
Bhargav <vyasb1503@gmail.com>
2025-07-23 20:49:55 +0530

    change the  size of the our advantage  and add the animation to the meet our team only for  the   mobile  and also change  in hte servie

--- src/pages/OurAdvantages.jsx
+++ src/pages/OurAdvantages.jsx
@@ -5,7 +5,8 @@ import internet from "../Assets/internet.png";
 import Peoples from "../Assets/Peoples.png";
 import Rocket from "../Assets/Rocket.png";
 import Circle from "../Assets/Circle.png";
-import GradientGlowText from "../Component/Typography/Heading"; 
+import GradientGlowText from "../Component/Typography/Heading";
+
 export default function OurAdvantages() {
   const advantages = useMemo(
     () => [
@@ -40,23 +41,32 @@ export default function OurAdvantages() {
     ],
     []
   );
+
   const leftColumn = [advantages[0], advantages[2]];
   const rightColumn = [advantages[1], advantages[3]];
+
   return (
-    <section id="OurAdvantages" className="bg-black text-white py-20 px-6 text-center">
-      <GradientGlowText className="text-xl sm:text-2xl md:text-5xl lg:text-6xl font-black mb-6 tracking-widest">
-        OUR ADVANTAGES
+    <section
+      id="OurAdvantages"
+      className="bg-black text-white py-20 px-6 text-center"
+    >
+      <GradientGlowText className="text-4xl sm:text-5xl md:text-6xl lg:text-[60px] font-black mb-6 tracking-widest leading-tight">
+        <span className="block sm:inline">OUR</span>{" "}
+        <span className="block sm:inline">ADVANTAGES</span>
       </GradientGlowText>
-      <h3 className="text-2xl sm:text-3xl md:text-5xl font-extrabold mb-4">
+
+      <h3 className="text-[48px] font-extrabold mb-4 leading-snug">
         Not Just Another Agency,
         <br />
         <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-500">
           A Real Growth Partner.
         </span>
       </h3>
+
       <p className="text-gray-400 max-w-2xl mx-auto mb-14 text-sm sm:text-base md:text-lg">
         Here's how we help Web3 and AI startups grow with purpose.
       </p>
+
       <div className="flex justify-center flex-col md:flex-row gap-6 items-center">
         <div className="flex flex-col gap-y-4 items-center">
           {leftColumn.map((item, index) => (
@@ -81,8 +91,8 @@ export default function OurAdvantages() {
           ))}
         </div>
       </div>
-      <a
-        href="https://calendly.com/gtmlabsxyz/consult" >
+
+      <a href="https://calendly.com/gtmlabsxyz/consult">
         <button className="mt-12 px-8 py-4 bg-gradient-to-r from-purple-600 to-blue-500 hover:from-purple-700 hover:to-blue-600 text-white font-semibold rounded-full transition-all duration-300 shadow-lg">
           Turn Strategy Into Action
         </button>
